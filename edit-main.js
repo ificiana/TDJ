f6169e3f729b4fd40f9ded8730b2a8700e062b53
ificiana <ificiana@gmail.com>
2023-01-10 12:59:30 +0530

    update scripts and styles

---
+++ edit-main.js
@@ -0,0 +1,109 @@
+const escapeHTML = s => {
+    let out = "";
+    let p2 = 0;
+    for (let p = 0; p < s.length; p++) {
+        let r;
+        switch (s.charCodeAt(p)) {
+            case 34:
+                r = "&quot;";
+                break; // "
+            case 38:
+                r = "&amp;";
+                break; // &
+            case 39:
+                r = "&#39;";
+                break; // '
+            case 60:
+                r = '&lt;';
+                break; // <
+            case 62:
+                r = '&gt;';
+                break; // >
+            default:
+                continue;
+        }
+        if (p2 < p) {
+            out += s.substring(p2, p);
+        }
+        out += r;
+        p2 = p + 1;
+    }
+    if (p2 == 0) {
+        return s;
+    }
+    if (p2 < s.length) {
+        out += s.substring(p2);
+    }
+    return out;
+}
+let area = null;
+let DATA = null;
+
+function editStart(dom, _t) {
+    area = document.createElement('textarea');
+    area.className = 'edit';
+    area.value = DATA[_t];
+    area.onkeydown = (e) => (e.key == 'Enter') ? this.blur() : () => {};
+    area.onblur = () => editEnd(dom, _t);
+    area.style.width = "100%";
+    area.style.height = "4rem";
+    dom.replaceWith(area);
+    area.focus();
+}
+
+function editEnd(dom, _t) {
+    DATA[_t] = escapeHTML(area.value);
+    edit_show_data(DATA);
+}
+
+function save(_done = 0) {
+    document.getElementById('id01')
+        .style.display = 'block';
+    if (_done) {
+        const form = document.forms.save_form;
+        const data = {
+            uname: form["uname"].value,
+            psw: form["psw"].value,
+            data: {
+                def: DATA.def,
+                sub: DATA.sub,
+                key: DATA.key
+            }
+        };
+        (async () => {
+            const rawResponse = await fetch(endpts.edit(), {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            const response = await rawResponse.json();
+            response.status == "success" ? window.location.href = `index.html?=${sq}` : alert("Something went wrong :( See logs.");
+            console.log(response);
+        })();
+    }
+}
+
+function edit_render(str) {
+    return str.replace(/\[!(.+?)\]/g, `<a href='/$1.html?=${sq}' target='_blank'>$1</a>`)
+        .replace(/\[(.+?)\]/g, "<a href='index.html?=$1' target='_blank'>$1</a>");
+}
+
+function edit_show_data(_json) {
+    if (_json.key == "404") {
+        _json.key = "Enter word";
+        _json.sub = "Enter subheading (can be empty)";
+        _json.def = "Enter definion, supports TDJ markdown";
+    }
+    DATA = _json;
+    const res = document.getElementById("main");
+    res.innerHTML = `<h1 onclick="editStart(this, 'key')">${_json.key}</h1><hr><h2 onclick="editStart(this, 'sub')">${_json.sub}</h2><p onclick="editStart(this, 'def')">${edit_render(_json.def)}</p>`;
+    document.getElementById("footer")
+        .innerHTML = `<hr>TDJ by Ificiana`;
+}
+async function load(query) {
+    const data = await fetch(endpts.get(query));
+    edit_show_data(await data.json(), query);
+}
--- main.js
+++ main.js
@@ -1,93 +1,119 @@
-const api_endpt = "https://ificiana.alwaysdata.net/"
+// const api_endpt = "https://ificiana.alwaysdata.net/?fun="
+// const api_endpt = "http://127.0.0.1:5000/?fun="
+const NAMESPACE = "6cea5068-1a3d-48fc-953d-5930604e7873";
+const api_endpt = `https://jp-tok.functions.appdomain.cloud/api/v1/web/${NAMESPACE}/default/app.json?fun=`;
+const endpts = {
+    get(k) {return `${api_endpt}tdj-get&k=${k}`},
+    search(q) {return `${api_endpt}tdj-search&q=${q}`},
+    edit() {return `${api_endpt}tdj-edit`}
+}
+const val_reject = /[.,\\\/#!+@$%\^&\*;\|:{}=\-_`~()0-9%]/g;
+const debounce = (fn, ms) => {
+    let timer = 0;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(fn.bind(this, ...args), ms || 0);
+    }
+}
+
 async function search(inp) {
-            arr = await (async () => {
-                const data = await fetch(`${api_endpt}tdj-search?q=${inp.value}`);
-                return data.json();
-            })();
-            var currentFocus;
-            inp.addEventListener("input", async function (e) {
-                var a,
-                    b,
-                    i,
-                    val = this.value;
+    let arrSearch = [], currentFocus;
+    inp.addEventListener("input", debounce(async function (e) {
+        let a, b, i, currentVal = this.value.replace(val_reject, "").substring(0,8);
+        closeAllLists();
+        if (!currentVal) return false;
+        const regenList = () => {
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "-search-list");
+            a.setAttribute("class", "search-items");
+            this.parentNode.appendChild(a);
+        }
+        const addToList = T => {
+            b = document.createElement("DIV");
+            b.innerHTML += T;
+            return b;
+        }
+        regenList();
+        a.appendChild(addToList("Loading..."));
+        arrSearch = !currentVal? []: await (async () => {
+            const data = await fetch(endpts.search(currentVal));
+            d = await data.json();
+            return await d["res"];
+        })();
+        closeAllLists();
+        regenList();
+        currentFocus = -1;
+        if (!arrSearch.length) {
+            a.appendChild(addToList("No match found."));
+            return;
+        }
+        for (i of arrSearch) {
+            b = addToList(i+"<input type='hidden' value='" + i + "'>")
+            b.addEventListener("click", function () {
+                inp.value = this.getElementsByTagName("input")[0].value;
                 closeAllLists();
-                if (!val) {
-                    return false;
-                }
-                arr = await (async () => {
-                    const data = await fetch(`${api_endpt}tdj-search?q=${val}`);
-                    return data.json();
-                })();
-                console.log(arr);
-                currentFocus = -1;
-                a = document.createElement("DIV");
-                a.setAttribute("id", this.id + "-search-list");
-                a.setAttribute("class", "search-items");
-                this.parentNode.appendChild(a);
-                for (i = 0; i < arr.length; i++) {
-                    b = document.createElement("DIV");
-                    b.innerHTML += arr[i];
-                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                    b.addEventListener("click", function (e) {
-                        inp.value = this.getElementsByTagName("input")[0].value;
-                        closeAllLists();
-                        window.location.href = `?=${inp.value}`;
-                    });
-                    a.appendChild(b);
-                }
+                window.location.href = `?=${inp.value}`;
             });
-            inp.addEventListener("keydown", function (e) {
-                var x = document.getElementById(this.id + "-search-list");
-                if (x) x = x.getElementsByTagName("div");
-                if (e.keyCode == 40) {
-                    currentFocus++;
-                    addActive(x);
-                } else if (e.keyCode == 38) {
-                    currentFocus--;
-                    addActive(x);
-                } else if (e.keyCode == 13) {
-                    e.preventDefault();
-                    if (currentFocus > -1) {
-                        if (x) x[currentFocus].click();
-                    }
-                }
-            });
-            function addActive(x) {
-                if (!x) return false;
-                removeActive(x);
-                if (currentFocus >= x.length) currentFocus = 0;
-                if (currentFocus < 0) currentFocus = x.length - 1;
-                x[currentFocus].classList.add("search-active");
+            a.appendChild(b);
+        }
+    }, 800));
+    inp.addEventListener("keydown", function (e) {
+        let x = document.getElementById(this.id + "-search-list");
+        if (x) x = x.getElementsByTagName("div");
+        switch(e.keyCode){
+            case 40: 
+                currentFocus++;
+                addActive(x);
+                return;
+            case 38:
+                currentFocus--;
+                addActive(x);
+                return;
+            case 13:
+                e.preventDefault();
+                if (currentFocus > -1) x[currentFocus].click();
+        }
+    });
+
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = x.length - 1;
+        x[currentFocus].classList.add("search-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("search-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("search-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
             }
-            function removeActive(x) {
-                for (var i = 0; i < x.length; i++) {
-                    x[i].classList.remove("search-active");
-                }
-            }
-            function closeAllLists(elmnt) {
-                var x = document.getElementsByClassName("search-items");
-                for (var i = 0; i < x.length; i++) {
-                    if (elmnt != x[i] && elmnt != inp) {
-                        x[i].parentNode.removeChild(x[i]);
-                    }
-                }
-            }
-            document.addEventListener("click", function (e) {
-                closeAllLists(e.target);
-            });
         }
-        function render(str, query) {
-            console.log(query)
-            return str.replace(/\[!(.+?)\]/g, `<a href='$1.html?=${query}'>$1</a>`).replace(/\[(.+?)\]/g, "<a href='?=$1'>$1</a>");
-        }
-        function show_data(_json, query) {
-            console.log(query)
-            const res = document.getElementById("main");
-            res.innerHTML = `<h1>${_json.key}</h1><hr><h2>${_json.sub}</h2><p>${render(_json.def, query)}</p>`;
-            document.getElementById("footer").innerHTML = `<hr>${render(_json.footer, query)}`;
-        }
-        async function load(query) {
-            console.log(query)
-            const data = await fetch(`${api_endpt}tdj-get?=${query}`);
-            show_data(await data.json(), query);
-        }
+    }
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+function render(str, query) {
+    return str.replace(/\[!(.+?)\]/g, `<a href='$1.html?=${query}'>$1</a>`)
+        .replace(/\[(.+?)\]/g,
+            "<a href='?=$1'>$1</a>");
+}
+
+function show_data(_json, query) {
+    const res = document.getElementById("main");
+    res.innerHTML = `<h1>${_json.key}</h1><hr><h2>${_json.sub}</h2><p>${render(_json.def, query)}</p>`;
+    document.getElementById("footer").innerHTML = `<hr>${render(_json.footer, query)}`;
+}
+async function load(query) {
+    const data = await fetch(endpts.get(query));
+    show_data(await data.json(), query);
+}
